3cad2c0ef353a14856ff5ec4ed4fe6ab04e078b9
Alexander Nelzin <asnelzin@gmail.com>
2016-10-19 19:13:03 +0300

    Add test refreshControl

--- Rainy/HomeWeatherViewController.swift
+++ Rainy/HomeWeatherViewController.swift
@@ -9,17 +9,37 @@
 import UIKit
 
 class HomeWeatherViewController: UIViewController {
+    
+    var refreshControl: UIRefreshControl!
+    
+    @IBOutlet weak var scrollView: UIScrollView!
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        refreshControl = UIRefreshControl()
+        refreshControl.addTarget(self, action: #selector(onRefresh), for: .valueChanged)
+        scrollView.insertSubview(refreshControl, at: 0)
+        
         // Do any additional setup after loading the view, typically from a nib.
     }
-
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
+    
+    // Implement the delay method
+    func run(after wait: TimeInterval, closure: @escaping () -> Void) {
+        let queue = DispatchQueue.main
+        queue.asyncAfter(deadline: DispatchTime.now() + wait, execute: closure)
+    }
+    
+    // Call the delay method in your onRefresh() method
+    func onRefresh() {
+        run(after: 2) {
+            self.refreshControl.endRefreshing()
+        }
+    }
 
 }
 
